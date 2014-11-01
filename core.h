bbdb941a322aced5183db0f5485d663e22aabcc0
kalin.eh <kalin.eh@gmail.com>
2014-11-01 20:56:00 +1000

    cast faff

--- core.h
+++ core.h
@@ -12,13 +12,18 @@
 typedef uint8_t mem8;
 typedef int32_t mem32;
 
+inline mem32 cast32_wrap(int x) { return *((mem32*)&x); } 
+inline mem32 cast32_wrap(float x) { return *((mem32*)&x); } 
+inline mem32 cast32_wrap(char x) { return *((mem32*)&x); } 
+inline mem32 cast32_wrap(void* x) { return *((mem32*)&x); } 
+
 #define ASSERT(cond, format, ...) if (!cond) { printf(format, __VA_ARGS__); assert(false); }
 #define ASSERTN(cond) if (!cond) { assert(false); }
 #define LOG(format, ...) printf(format, __VA_ARGS__); printf("\n");
 #define LOGN(format) printf(format ); printf("\n");
 
 #define countof(a) (sizeof(a) / sizeof(a[0]))
-#define cast32(a) (reinterpret_cast<mem32>(a & 0x7FFFFFFF))
+#define cast32(a) cast32_wrap(a)
 #define write32(dst, val) { \
 	mem32 inner = val; \
 	memcpy(dst, &inner, sizeof(mem32)); \
